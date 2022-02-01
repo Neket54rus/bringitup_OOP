f685e11c3f086db36ee3ca7e1c18bc5ae28b2af1
‘Nikita’ <kvar228@mail.ru>
2022-02-01 14:43:12 +0700

    implementation of secondary sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,37 @@
 import VideoPlayer from "./modules/playVideo";
 import MainSlider from "./modules/slider/slider-main";
+import MiniSlider from "./modules/slider/slider-min"
 
 window.addEventListener('DOMContentLoaded', () => {
-	const slider = new MainSlider({btnsSelector: '.next', pageSelector: '.page'})
+	const slider = new MainSlider({btnsSelector: '.next', wrapperForSliderSelector: '.page'})
 	slider.render()
+
+	const showUpSlider = new MiniSlider({
+		wrapperForSliderSelector: '.showup__content-slider',
+		prevBtnSelector: '.showup__prev',
+		nextBtnSelector: '.showup__next',
+		activeClass: 'card-active',
+		animate: true
+	})
+	showUpSlider.init()
+
+	const modulesSlider = new MiniSlider({
+		wrapperForSliderSelector: '.modules__content-slider',
+		prevBtnSelector: '.modules__info-btns .slick-prev',
+		nextBtnSelector: '.modules__info-btns .slick-next',
+		activeClass: 'card-active',
+		animate: true,
+		autoplay: true
+	})
+	modulesSlider.init()
+
+	const feedSlider = new MiniSlider({
+		wrapperForSliderSelector: '.feed__slider',
+		prevBtnSelector: '.feed__slider .slick-prev',
+		nextBtnSelector: '.feed__slider .slick-next',
+		activeClass: 'feed__item-active'
+	})
+	feedSlider.init()
 
 	const player = new VideoPlayer('.showup .play', '.overlay')
 	player.init()
--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,8 +1,21 @@
 export default class Slider {
-	constructor({pageSelector = '', btnsSelector = '', nextBtnSelector = '', prevBtnSelector = ''} = {}) {
-		this.page = document.querySelector(pageSelector)
-		this.slides = this.page.children
+	constructor({
+						wrapperForSliderSelector = null,
+						btnsSelector = null,
+						nextBtnSelector = null,
+						prevBtnSelector = null,
+						activeClass = '',
+						animate,
+						autoplay
+					} = {}) {
+		this.wrapper = document.querySelector(wrapperForSliderSelector)
+		this.slides = this.wrapper.children
 		this.btns = document.querySelectorAll(btnsSelector)
+		this.prev = document.querySelector(prevBtnSelector)
+		this.next = document.querySelector(nextBtnSelector)
+		this.activeClass = activeClass
+		this.animate = animate
+		this.autoplay = autoplay
 		this.slideIndex = 1
 	}
 }
